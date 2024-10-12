46e8715142fa8fd4f7ca06b3c0e3efe8c3602326
Ogwen123 <owen.d.jones@btinternet.com>
2024-10-12 18:03:10 +0100

    detect things that might be powers and add in a ^

--- main.py
+++ main.py
@@ -11,15 +11,33 @@ map = {
     "∃": "\\exists "
 }
 
+variables = "xXyY"
+
+ASSUME_POWERS = True
+
 while True:
     buffer = ""
     text = input(">")
-    for letter in text:
+    pos = 0
+    while pos < len(text):
+        letter = text[pos]
         if text == "exit": exit()
         if letter in dict.keys(map):
             buffer += map[letter]
+        elif letter in variables and ASSUME_POWERS:
+            num = ""
+            while True:
+                if (pos + len(num) + 1) < len(text) and text[pos + len(num) + 1].isnumeric():
+                    num += text[pos + len(num) + 1]
+                else: break
+            buffer += letter if len(num) == 0 else letter + "^" + num
+            pos += len(num) + 1
+            if not len(num) == 0: print(f"WARNING: assumed that {letter}{num} was meant to be a power and added a ^")
+            continue
         else:
             buffer += letter
+        pos += 1
+        
     buffer = f"${buffer}$"
     print(buffer)
     subprocess.run(['clip.exe'], input=buffer.encode('UTF-16LE'), check=True)
