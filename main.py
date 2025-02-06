1885e270c0b49146e98f12c77ce6b226366f619e
Ogwen123 <owen.d.jones@btinternet.com>
2025-02-06 10:26:30 +0000

    config things

--- main.py
+++ main.py
@@ -6,18 +6,32 @@ map = {
     "∧": "\\land ",
     "→": "\\implies ",
     "∀": "\\forall ",
-    "Z": "\\mathbf{Z} ",
     "∈": "\\in ",
-    "∃": "\\exists "
+    "∃": "\\exists ",
+    "≡": "\\equiv "
 }
 
 variables = "xXyY"
 
+ESCAPE_SPACES = FALSE
 ASSUME_POWERS = True
+ADD_DOLLAR_SIGNS = False
+REPLACE_Z_WITH_INTEGER_SYMBOL = False
+
+if REPLACE_Z_WITH_INTEGER_SYMBOL: map["Z"] = "\\mathbf{Z} "
 
 while True:
     buffer = ""
     text = input(">")
+    if text == "ES":
+        ESCAPE_SPACES = not ESCAPE_SPACES
+        continue
+    elif text == "AP":
+        ASSUME_POWERS = not ASSUME_POWERS
+        continue
+    elif text == "DS":
+        ADD_DOLLAR_SIGNS = not ADD_DOLLAR_SIGNS
+        continue
     pos = 0
     while pos < len(text):
         letter = text[pos]
@@ -34,10 +48,12 @@ while True:
             pos += len(num) + 1
             if not len(num) == 0: print(f"WARNING: assumed that {letter}{num} was meant to be a power and added a ^")
             continue
+        elif ESCAPE_SPACES and letter == " ":
+            buffer += "\ "
         else:
             buffer += letter
         pos += 1
         
-    buffer = f"${buffer}$"
+    buffer = f"${buffer}$" if ADD_DOLLAR_SIGNS else buffer
     print(buffer)
     subprocess.run(['clip.exe'], input=buffer.encode('UTF-16LE'), check=True)
